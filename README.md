8bd1ca72a1fdaa48a06ad96d7bf64f654f492dbe
vloison <virg.loison@gmail.com>
2023-11-16 12:37:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# LoC-RoC: Automated detection of Loss and Return of Consciousness based on electro-encephalograms
+
+This repo contains an off-the-shelf method to automatically detect times of loss of consciousness (LoC) and return of consciousness (RoC) on intraoperative EEG during general anesthesia. 
+
+The detection is made by fitting an activation function to the outputs of a a gradient boosting model trained on the PSD of 30-seconds epochs of EEG signal, see [paper](https://ieeexplore.ieee.org/abstract/document/10199018). 
+
+
+## Installation
+First, ensure that you have downloaded the boost-loc-roc folder containing this README and that you have an up-to-date version of ```conda```. 
+
+We recommend creating a dedicated virtual environment using ```conda```. This will install all necessary Python packages in their correct versions. 
+```
+cd <this_folder>
+conda env create -n loc-roc --file environment.yml
+```
+
+## Quickstart
+See `quickstart.ipynb`.
+
+## Citing this work
+If this package was useful to you, please cite it: \
+O. S. Aubin, I. Khemir, J. Perdereau, C. Touchard, F. Vallée and J. Cartailler, "Repurposing electroencephalographic signal for automatic segmentation of intra-operative periods under general anesthesia," IEEE EUROCON 2023 - 20th International Conference on Smart Technologies, Torino, Italy, 2023, pp. 286-290, doi: 10.1109/EUROCON56442.2023.10199018.
+```
+@INPROCEEDINGS{aubin2023repurposing,
+  author={Aubin, O. Saint and Khemir, I. and Perdereau, J. and Touchard, C. and Vallée, F. and Cartailler, J.},
+  booktitle={IEEE EUROCON 2023 - 20th International Conference on Smart Technologies}, 
+  title={Repurposing electroencephalographic signal for automatic segmentation of intra-operative periods under general anesthesia}, 
+  year={2023},
+  volume={},
+  number={},
+  pages={286-290},
+  doi={10.1109/EUROCON56442.2023.10199018}}
+  ```
---
+++ boost_loc_roc/model.py
@@ -0,0 +1,66 @@
+"""Module to define the GB and voting ensemble model."""
+from sklearn.ensemble import VotingClassifier, GradientBoostingClassifier
+from sklearn.preprocessing import LabelEncoder
+
+import os.path as op
+from joblib import load
+import numpy as np
+
+
+
+def get_models(weights_dir,template, n_splits):
+    """Choose the model."""
+    models = [load(op.join(weights_dir, f"{template}{i}.pkl")) for i in range(n_splits)]
+    return models
+
+
+def create_model(
+    n_estimators=138,
+    learning_rate=0.24210526315789474,
+    min_samples_leaf=6,
+    max_depth=2,
+    min_samples_split=2,
+    random_state=42,
+    max_features=None,
+):
+    """Create a GB model."""
+    model = GradientBoostingClassifier(
+        n_estimators=n_estimators,
+        learning_rate=learning_rate,
+        min_samples_leaf=min_samples_leaf,
+        max_depth=max_depth,
+        min_samples_split=min_samples_split,
+        random_state=random_state,
+        max_features=max_features,
+    )
+    return model
+
+
+def create_voting_ensemble_model(
+    clf_list, weights_dir, y_train = None, voting="soft", n_jobs=-1, verbose=True
+):
+    """Create a voting ensemble model."""
+    voting_classifier = VotingClassifier(
+        estimators=[
+            ("gbc1", clf_list[0]),
+            ("gbc2", clf_list[1]),
+            ("gbc3", clf_list[2]),
+        ],
+        voting=voting,
+        n_jobs=n_jobs,
+        verbose=verbose,
+    )
+
+    voting_classifier.estimators_ = clf_list
+
+    if y_train is not None : 
+        voting_classifier.le_ = LabelEncoder().fit(y_train)
+        np.save(op.join(weights_dir, 'voting_classifier_weight.npy'), voting_classifier.le_.classes_)
+        voting_classifier.classes_ = voting_classifier.le_.classes_
+
+    else : 
+        voting_classifier.le_ = LabelEncoder()
+        voting_classifier.classes_ = np.load(op.join(weights_dir, 'voting_classifier_weight.npy'))
+        voting_classifier.le_.classes_ = voting_classifier.classes_
+
+    return voting_classifier
