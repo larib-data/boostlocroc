34737d0005d845f23ca3026e87664551c7713a73
vloison <virg.loison@gmail.com>
2024-11-08 12:07:04 +0100

    clean quickstart, move npy file to archive

--- boostlocroc/vis.py
+++ boostlocroc/vis.py
@@ -4,13 +4,13 @@ Functions
     plot_spectrogram
 """
 
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.gridspec as gridspec
 
 
 def plot_spectrogram(time_loc, time_roc, signal, sfreq, time, t_proba, proba):
-    """Plots the spectrogram , estimated LoC/RoC times and per epoch LoC/RoC
+    """Plots the spectrogram, estimated LoC/RoC times and per epoch LoC/RoC
     probability.
 
     Parameters
@@ -43,27 +43,28 @@ def plot_spectrogram(time_loc, time_roc, signal, sfreq, time, t_proba, proba):
     ax2 = fig.add_subplot(gs[2], sharex=ax0)
 
     ax = [ax0, ax1, ax2]
-    ax[0].axvline(x=time_loc, color='r', linestyle='--', linewidth=3)
-    ax[0].axvline(x=time_roc, color='r', linestyle='--', linewidth=3)
+    ax[0].axvline(x=time_loc, color="r", linestyle="--", linewidth=3)
+    ax[0].axvline(x=time_roc, color="r", linestyle="--", linewidth=3)
 
-    nperseg = np.floor(1.5*sfreq).squeeze()
-    noverlap = np.floor(nperseg/3).squeeze()
+    nperseg = np.floor(1.5 * sfreq).squeeze()
+    noverlap = np.floor(nperseg / 3).squeeze()
     # Plot spectrogram
     pxx, freqs, bins, im = ax[0].specgram(
         signal,
         Fs=sfreq,
         NFFT=int(nperseg),
-        mode='psd',
-        cmap='jet',
-        noverlap=int(noverlap))
+        mode="psd",
+        cmap="jet",
+        noverlap=int(noverlap),
+    )
 
     im.set_clim(-20, 25)
     # Add lines at predicted times of LoC and RoC
     ax[1].plot(time, signal)
-    ax[1].axvline(x=time_loc, color='r', linestyle='--', linewidth=3)
-    ax[1].axvline(x=time_roc, color='r', linestyle='--', linewidth=3)
+    ax[1].axvline(x=time_loc, color="r", linestyle="--", linewidth=3)
+    ax[1].axvline(x=time_roc, color="r", linestyle="--", linewidth=3)
     # Plot classification probabilities
     ax[2].scatter(t_proba, proba)
-    ax[2].set_xlabel('Time')
-    ax[2].set_ylabel('Proba')
+    ax[2].set_xlabel("Time")
+    ax[2].set_ylabel("Proba")
     plt.show()
